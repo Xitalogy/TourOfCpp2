f816f5de35cb64d9c82a5b06c0ca871e5ba23b50
Xitalogy <1267648+Xitalogy@users.noreply.github.com>
2020-03-06 19:19:05 -0600

    CAndCppPointers: reworked snippets; compiling

--- Chapter1/CAndCppPointers/CAndCppPointers/CAndCppPointers.cpp
+++ Chapter1/CAndCppPointers/CAndCppPointers/CAndCppPointers.cpp
@@ -6,6 +6,8 @@ void CharVarTest() {
   c = 'g'; // assign the character 'g' to variable c
 
   printf("%c", c); // Output: g
+
+  std::cout << std::endl;
 }
 
 void IntVarTest() {
@@ -13,11 +15,18 @@ void IntVarTest() {
   i = 15339; // assign the integer value 15339 to i
 
   printf("%d", i); // Output: 15339
+
+  std::cout << std::endl;
 }
 
 void DefiningPointersTest() {
   char* pc; // Define a variable named pc of type "pointer to char"
   int* pi;  // Define a variable named pi of type "pointer to int"
+
+  // Output the addresses of the pointer variables pc and pi
+  printf("%p %p", &pc, &pi); // output 0C 09
+
+  std::cout << std::endl;
 }
 
 void AssigningValuesToPointersTest() {
@@ -30,23 +39,11 @@ void AssigningValuesToPointersTest() {
   pc = &c;  // Get the address of the c and put it in pc
   pi = &i;  // Get the address of the i and put it in pi
 
-  // Print out the addresses stored in pc and pi
-  printf("%p %p", pc, pi); // Output: 02 04
-}
-
-void PointerAddressesTest() {
-  char c;    // Define a char variable
-  int i;     // Define an int variable
-
-  char* pc; // Define a variable of type "pointer to char"
-  int* pi;  // Define a variable of type "pointer to int"
-
-  pc = &c;  // Get the address of c and put it in pc
-  pi = &i;  // Get the address of i and put it in pi
-  
-  // Print out the addresses then contents of the variables pc and pi
+  // Print out the pointer addresses of and addresses stored in pc and pi
   printf("%p %p %p %p", &pc, &pi, pc, pi);
   // Output: 0C 09 02 04
+
+  std::cout << std::endl;
 }
 
 void UsingPointersTest() {
@@ -54,8 +51,9 @@ void UsingPointersTest() {
   char* pc = &c; // define "pointer to char" variable pc
                  // and give it the value: address of c
 
-  printf("%c %p %p %c", c, &pc, pc, *pc);
-  // Output: g 006FFBC0 006FFBCF g
+  printf("%c %c", c, *pc); // Output: g g
+  
+  std::cout << std::endl;
 }
 
 void ChangingPointersTest() {
@@ -68,16 +66,22 @@ void ChangingPointersTest() {
                   // and assign it the value <address of d>
 
   printf("%c %c %p %p %c %c", c, d, pc, pd, *pc, *pd);
-  // Output: g f 006FFBCF 006FFBC3 g f
+  // Output: g f 02 04 g f
+
+  std::cout << std::endl;
 
   pd = pc; // Assign value in pc to pd
 
   printf("%p %p %c %c", pc, pd, *pc, *pd);
-  // Output: 006FFBCF 006FFBCF g g
+  // Output: 02 02 g g
+
+  std::cout << std::endl;
 
   pc = &d; // Assign address of char variable d to pc
   printf("%p %p %c %c", pc, pd, *pc, *pd);
-  // Output: 006FFBC3 006FFBCF f g
+  // Output: 04 02 f g
+
+  std::cout << std::endl;
 }
 
 void AssigningValuesUsingPointersTest() {
@@ -85,28 +89,26 @@ void AssigningValuesUsingPointersTest() {
   char* pc = &c; // Define pointer-to-char variable pc
                  // and assign the value: <address of c>
 
-  printf("%c", c); // Output: g
+  printf("%c", c); // print out value stored in c
+ // Output: g
+
+  std::cout << std::endl;
 
   *pc = 'f'; // Using the dereferenced pointer pc, assign c the value: f
 
-  printf("%c", c); // Output: f
+  // Print out the value stored in c directly then with dereferenced pc
+  printf("%c %c", c, *pc); // Output: f f
+
+  std::cout << std::endl;
 }
 
 int main()
 {
-  std::cout << "CharVarTest():" << std::endl;
   CharVarTest();
-  std::cout << std::endl << std::endl << "IntVarTest:" << std::endl;
   IntVarTest();
-  std::cout << std::endl << std::endl << "DefiningPointersTest():" << std::endl;
   DefiningPointersTest();
-  std::cout << std::endl << std::endl << "AssigningValuesToPointersTest:" << std::endl;
   AssigningValuesToPointersTest();
-  std::cout << std::endl << std::endl << "UsingPointersTest():" << std::endl;
   UsingPointersTest();
-  std::cout << std::endl << std::endl << "ChangingPointersTest():" << std::endl;
   ChangingPointersTest();
-  std::cout << std::endl << std::endl << "AssigningValuesUsingPointersTest():" << std::endl;
   AssigningValuesUsingPointersTest();
-  std::cout << std::endl;
 }
